90f949e0208698984c233c145d1f8d2ee7a9f4c7
Nguyen Thai Huy <hamo.thaihuy@gmail.com>
2024-04-01 17:34:54 +0700

    call api transaction oke

--- routers/money.router.js
+++ routers/money.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const moneyModal = require("../Models/money.modal");
 const { startSession } = require("mongoose");
+const { testConnection } = require("../Helpers/connect.multi.mongodb");
+const createHttpError = require("http-errors");
 const router = express.Router();
 
 router.post("/v1/api/user", async (req, res, next) => {
@@ -17,28 +19,24 @@ router.post("/v1/api/user", async (req, res, next) => {
 })
 // {"_id": "mongoRepSet","members": [{"_id": 0, "host": "172.26.0.2:27017"}, {"_id": 1, "host": "172.26.0.3:27017"}, {"_id": 2, "host": "172.26.0.4:27017"}]}
 router.post("/v1/api/transfer", async (req, res, next) => {
-    const session = await startSession()
+    const session = await testConnection.startSession()
     try {
         const { fromId, toId, amount } = req.body;
-        console.log(req.body);
-        // session.startTransaction();
-        const transactionOptions = {
-            readPreference: 'primary',
-            readConcern: { level: 'local' },
-            writeConcern: { w: 'majority' }
-          };
-        await session.withTransaction(async () => {
-            const amountFrom = await moneyModal.findOneAndUpdate({ userId: +fromId }, {
-                $inc: { amount: -amount }
-            }, { session, new: true })
-            console.log(`Acount ${fromId} is:::`, amountFrom);
-            const amountTo = await moneyModal.findOneAndUpdate({ userId: +toId }, {
-                $inc: { amount: amount }
-            }, { session, new: true })
-            console.log(`Acount ${toId} is:::`, amountTo);
-        }, transactionOptions);
+        session.startTransaction();
+        const amountFrom = await moneyModal.findOneAndUpdate({ userId: +fromId }, {
+            $inc: { amount: -amount }
+        }, { session, new: true })
+        if (amountFrom.amount < 0) {
+            throw createHttpError.BadRequest("User not enough money!!");
+        }
+
+        const amountTo = await moneyModal.findOneAndUpdate({ userId: +toId }, {
+            $inc: { amount: amount }
+        }, { session, new: true })
+
         await session.commitTransaction()
         session.endSession()
+        
         return res.json({
             status: "OK",
             data: {
--- test.js
+++ test.js
@@ -1,56 +1,16 @@
-const dataTest = {
-    '0': [
-        {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            name: 'string',
-            parentId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            level: 0,
-            documents: [],
-        },
-    ],
-    '1': [
-        {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            name: 'string',
-            parentId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            level: 1,
-            documents: [],
-        },
-        {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66afa7',
-            name: 'string',
-            parentId: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            level: 1,
-            documents: [],
-        },
-    ],
-    '2': [
-        {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66asml',
-            name: 'string',
-            parentId: '3fa85f64-5717-4562-b3fc-2c963f66afa7',
-            level: 2,
-            documents: [],
-        },
-    ],
-};
-
-console.log();
-
-function test(objs) {
-    const arr = Object.keys(objs)
-    let obj
-    let data
-    obj = objs[arr[0]]
-    data = obj
-    for(let i = 1; i < arr.length; i ++) {
-        obj = obj.map(item => {
-            item.childen = objs[arr[i]]
-        })
-        obj = objs[arr[i]]
+Array.prototype.myForEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback.call( this[i], i, this)
     }
-
-    return data
 }
 
-console.log(test(dataTest)[0].childen[1].childen);
+const arr = [1, 2, 3, 4, 5, 6, 7]
+
+
+const forEachLoop = () => {
+    arr.myForEach((item) => {
+        console.log(item);
+    })
+}
+
+forEachLoop()
